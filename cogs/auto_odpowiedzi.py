726216250d5d6b91552f36ba4ba51dcec0feeef7
jasiusheys <potkajanek@gmail.com>
2026-06-14 18:31:12 +0200

    Update auto_odpowiedzi.py

--- cogs/auto_odpowiedzi.py
+++ cogs/auto_odpowiedzi.py
@@ -6,13 +6,11 @@ class AutoOdpowiedzi(commands.Cog):
         self.bot = bot
         # Klucze muszą być dokładnie takie, jakie mają zostać napisane (tylko małe litery)
         self.odpowiedzi = {
-            "xd": "nie śmiej się tyle, bo ci żyłka pęknie",
+            ,
             "dobranoc": "śpij dobrze, ale wróć jutro!",
             "nuda": "idź się pobaw na kanałach forfun",
-            "lagi": "kup se neta haha",
             "kiedy event": "wszystko masz na kanale rekrutacja nagrywki",
             "co": "jajco 1-0",
-            "siema": "siemanko co tam?",
             "sigma": "ty skibidi"
         }
 
